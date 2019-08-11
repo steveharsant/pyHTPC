74f9397f86ff385bebaca7f7b62ca0e7c96e0b81
Steve Harsant <stevenharsant@gmail.com>
2019-08-11 12:30:11 +0100

    minor updates

--- pyHTPC.py
+++ pyHTPC.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 # Get time
 sys_time = jsonify
 
+# Build tiles configuration dictionary
 tiles_config = '['
 tiles_count = 0
 
-# Build tiles configuration dictionary
 tiles_dir = './plugins/tiles'
 for tile in glob.glob(os.path.join(tiles_dir, '*.json')):
     with open(tile, 'r') as file:
@@ -21,8 +21,23 @@ for tile in glob.glob(os.path.join(tiles_dir, '*.json')):
 
 tiles_config = tiles_config[:-1]
 tiles_config = tiles_config + ']'
-tiles_config = json.loads(tiles_config)      
+tiles_config = json.loads(tiles_config)
+
+# Build navbar configuration dictionary
+navbar_config = '['
+navbar_count = 0
+
+navbar_dir = './plugins/navbar'
+for nav in glob.glob(os.path.join(navbar_dir, '*.json')):
+    with open(nav, 'r') as file:
+        contents = file.read()
+        navbar_config += contents + ','
+        navbar_count += 1
+
+navbar_config = navbar_config[:-1]
+navbar_config = navbar_config+ ']'
+navbar_config = json.loads(navbar_config)
 
 @app.route("/")
 def home():
-    return render_template('index.html', tiles_config=tiles_config, tiles_count=tiles_count)
+    return render_template('index.html', tiles_config=tiles_config, tiles_count=tiles_count, navbar_config=navbar_config)
